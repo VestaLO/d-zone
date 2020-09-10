f2096caf3ee8b511ce65cd2f010c4a325d151a3c
vegeta897 <vegeta897@users.noreply.github.com>
2020-09-10 06:28:47 +0000

    Deploying to gh-pages from  @ 335ff25090a9c87e82713c80ef0d248a00f55ee4 🚀

--- build/index-9c7f8c5a.js
+++
@@ -1,2 +0,0 @@
-import{r as e}from"./index.bundle.js";export default()=>{const l=e.lazy(()=>import("./index-47ec266e.js"));return e.createElement("div",null,e.createElement("div",{id:"game-wrapper"},e.createElement(e.Suspense,{fallback:e.createElement("p",null,"Loading")},e.createElement(l,null))))};
-//# sourceMappingURL=index-9c7f8c5a.js.map
